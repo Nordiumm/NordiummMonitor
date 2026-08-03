25e758f152fdeb6e9f96d33e19dc9eb3bc8c089a
Nordiumm <nordiumm.java@gmail.com>
2026-08-04 00:58:08 +0200

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+
+    eframe::ruin_native(
+        "Nordiumm Monitor",
+        options,
+        Box::new(|_cc| Ok(Box::new(NordiummMonitor::default()))),
+    )
 }
